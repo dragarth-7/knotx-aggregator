57189b0993fdc92cabb8e7180dad3f8bbb1519ac
Tomasz Michalak <tomek.michalak@gmail.com>
2019-07-30 15:26:22 +0200

    CI update

--- development/pull-all.sh
+++ development/pull-all.sh
@@ -67,7 +67,7 @@ checkout() {
     fi
     git --git-dir=$2/.git --work-tree=$2 fetch
   else
-    if [[ $AUTH_HEADER ]]; then
+    if [[ -z "$AUTH_HEADER" ]]; then
       echo "CI clone option with token $AUTH_HEADER"
       git -c http.extraheader="AUTHORIZATION: bearer $AUTH_HEADER" clone "https://github.com:$1/$2.git"
     else
